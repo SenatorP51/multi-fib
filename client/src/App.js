08d4c3be98e3de9be411662d75f2c63b245c30d3
SenatorP51 <spg@spg51.net>
2022-01-11 01:28:22 +0100

    next test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reac 2222         </a>
+            Learn Reac 11.01.2022         </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
